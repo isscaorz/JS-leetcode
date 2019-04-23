4c1f3b7c0e2644bb8d0d81e610127073b6c6342e
kongty <kongty@chint.com>
2019-04-23 19:10:56 +0800

    upto 0033

--- answer/0032. 最长有效括号.js
+++ answer/0032. 最长有效括号.js
@@ -19,5 +19,26 @@ https://leetcode-cn.com/problems/longest-valid-parentheses/
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    
+	let len = s.length;
+	if (len < 2) return 0;
+	let maxLen = 0;
+	let start = 0;
+	let sArr = [];
+	for (let i = 0; i < len; i++) {
+		if (s[i] == '(') {
+			sArr.push(i);
+		} else {
+			if (sArr.length == 0) {
+				start = i + 1;
+			} else {
+				sArr.pop();
+				if (sArr.length == 0) {
+					maxLen = Math.max(maxLen, i - start + 1);
+				} else {
+					maxLen = Math.max(maxLen, i - sArr[sArr.length - 1]);
+				}
+			}
+		}
+	}
+	return maxLen;
 };
--- waitfor/0034. 在排序数组中查找元素的第一个和最后一个位置.js
+++ waitfor/0034. 在排序数组中查找元素的第一个和最后一个位置.js
@@ -20,5 +20,21 @@ https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sort
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    
+	let len = nums.length;
+	
+	return -1;
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+	let idx = [];
+	nums.forEach(function(item, index, array) {
+		if (item === target) idx.push(index);
+	});
+	if (!idx.length) return [-1, -1];
+	return [idx[0], idx[idx.length - 1]];
 };
